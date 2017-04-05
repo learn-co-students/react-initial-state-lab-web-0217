96cac0bc358755b08abec00458a7d8045102281f
dgwp-projects <zayneabraham1@gmail.com>
2017-04-05 16:49:27 -0400

    Done.

---
+++ components/Bomb.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class Bomb extends React.Component{
+	constructor(props){
+		super();
+
+		this.state = {
+			secondsLeft: props.initialCount,
+		}
+	}
+
+	render(){
+		
+		const boom = this.state.secondsLeft > 0 ? `${this.state.secondsLeft} seconds left before I go boom!` : "Boom!";
+		
+		return(
+			<div>{boom}</div>
+		);
+	}
+}
+
