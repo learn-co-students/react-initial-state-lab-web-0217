3f3eedc8b27ff2de3b9f91fba4760fcdbd636b8d
github username <meingorn@gmail.com>
2017-04-04 21:08:17 -0400

    Done.

---
+++ components/Bomb.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+class Bomb extends React.Component {
+
+  constructor(props) {
+    super()
+    this.state = {
+      secondsLeft: props.initialCount
+    }
+  }
+
+  render() {
+    let countdownMsg = (this.state.secondsLeft == 0) ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`
+    return (
+      <div className="countDownArea">
+       {countdownMsg}
+       </div>
+    )
+  }
+}
+
+
+module.exports = Bomb
