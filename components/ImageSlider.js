8995325b0200812251dc7b1c3edad1d1111e85f0
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-04-06 09:57:17 -0400

    Done.

---
+++ components/ImageSlider.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+export default class ImageSlider extends React.Component {
+constructor(){
+  super()
+   this.state = {
+        currentSlideIndex: 0
+    };
+}
+
+
+ render() {
+      return (
+
+          <div>
+            <p>I am on slide {this.state.currentSlideIndex}</p>
+          </div>
+
+
+        );
+    }
+ }
